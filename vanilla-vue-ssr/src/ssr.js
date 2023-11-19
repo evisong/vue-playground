2ad1b5d99f467decb13384faa4d4f80de635817b
Yiwei Song <ywsong@freewheel.tv>
2023-11-19 17:48:37 +0800

    ssr cli accept arg

--- vanilla-vue-ssr/src/ssr.js
+++ vanilla-vue-ssr/src/ssr.js
@@ -5,9 +5,14 @@ import { renderToString } from 'vue/server-renderer'
 
 import App from './App.js'
 import router from './router/index.js'
+import process from 'process'
+
+const rootPath = process.argv[2] || '/'
 
 const app = createSSRApp(App)
 app.use(router)
+await router.push(rootPath)
+await router.isReady()
 
 renderToString(app).then((html) => {
   console.log(html)
